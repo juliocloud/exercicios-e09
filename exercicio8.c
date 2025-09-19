6c332ceb58557a0fdd2c8b321a2e24cecacce106
Júlio César <56365230+juliocloud@users.noreply.github.com>
2025-09-19 20:05:24 -0300

    exercicio8

---
+++ exercicio8.c
@@ -0,0 +1,15 @@
+#include <avr/io.h>
+
+int main(void) {
+    DDRD &= ~0b00000100;
+    DDRD |= 0b00001000;
+    PORTD |= 0b00001000;
+
+    while (1) {
+        if (PIND & 0b00000100) {
+            PORTD &= ~0b00001000;
+        }
+    }
+
+    return 0;
+}
